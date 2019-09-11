eeaf5d86eaeb7662825e9fbecd847918c63fcb2f
Iqbal_Mubarok <muhammadiqbalmubarok@gmail.com>
2019-09-11 09:54:43 +0700

    tambah activity & intent

--- app/src/main/java/com/example/data_mahasiswa/MainActivity.java
+++ app/src/main/java/com/example/data_mahasiswa/MainActivity.java
@@ -2,13 +2,46 @@ package com.example.data_mahasiswa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button lihatdata, inputdata, informasi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lihatdata = findViewById(R.id.btnLihatData);
+        inputdata = findViewById(R.id.btnInputData);
+        informasi = findViewById(R.id.btnInformasi);
+
+        lihatdata.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent lihatData = new Intent(MainActivity.this, ListDataMahasiswa.class);
+                startActivity(lihatData);
+            }
+        });
+
+        inputdata.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent inputData = new Intent(MainActivity.this, InputData.class);
+                startActivity(inputData);
+            }
+        });
+
+        informasi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent Informasi = new Intent(MainActivity.this, Informasi.class);
+                startActivity(Informasi);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/data_mahasiswa/splash.java
+++ app/src/main/java/com/example/data_mahasiswa/splash.java
@@ -1,14 +1,26 @@
 package com.example.data_mahasiswa;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class splash extends AppCompatActivity {
+
+    private int waktu = 4000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent home = new Intent(splash.this, MainActivity.class);
+                startActivity(home);
+                finish();
+            }
+        }, waktu);
     }
 }
