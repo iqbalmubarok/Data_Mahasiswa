8866c5dc5c7af76fb86ff1f3c8fce0b8be2b8e82
Iqbal_Mubarok <muhammadiqbalmubarok@gmail.com>
2019-09-14 21:23:29 +0700

    Penambahan semuanya kecuali databasehelper + recycleradapter

--- app/src/main/java/com/example/data_mahasiswa/DetailData.java
+++ app/src/main/java/com/example/data_mahasiswa/DetailData.java
@@ -3,12 +3,34 @@ package com.example.data_mahasiswa;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class DetailData extends AppCompatActivity {
+
+    EditText edtNo, edtNama, edtTgl, edtJenkel, edtAlamat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_data);
+
+        edtNo = findViewById(R.id.txtNo);
+        edtNama = findViewById(R.id.txtNama);
+        edtTgl = findViewById(R.id.txtTgl);
+        edtJenkel = findViewById(R.id.txtJenkel);
+        edtAlamat = findViewById(R.id.txtAlamat);
+
+        edtNo.setFocusable(false);
+        edtNama.setFocusable(false);
+        edtTgl.setFocusable(false);
+        edtJenkel.setFocusable(false);
+        edtAlamat.setFocusable(false);
+
+        Person person = getIntent().getParcelableExtra("DETAIL_INTENT");
+        edtNo.setText(String.valueOf(person.getNomor()));
+        edtNama.setText(person.getNama());
+        edtTgl.setText(person.getTanggal_lahir());
+        edtJenkel.setText(person.getJenis_kelamin());
+        edtAlamat.setText(person.getAlamat());
     }
 }
--- app/src/main/java/com/example/data_mahasiswa/InputData.java
+++ app/src/main/java/com/example/data_mahasiswa/InputData.java
@@ -2,14 +2,81 @@ package com.example.data_mahasiswa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class InputData extends AppCompatActivity {
+
+    EditText edtNo, edtName, edtTanggal, edtJenis, edtAlamat;
+    Button btnSubmit;
+    Context context;
+    Person updated;
+    String aksi, nomor = "Submit";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input_data);
+        context = this;
 
+        aksi = getIntent().getStringExtra("UPDATE_ACTION");
+        updated = getIntent().getParcelableExtra("UPDATE_INTENT");
+        if (aksi == null){
+            aksi = "Submit";
+        }else {
+            nomor = String.valueOf(updated.getNomor());
+        }
+
+        edtNo = findViewById(R.id.edtNomor);
+        edtName = findViewById(R.id.edtNama);
+        edtTanggal = findViewById(R.id.edtTglLahir);
+        edtJenis = findViewById(R.id.edtJenkel);
+        edtAlamat = findViewById(R.id.edtAlamat);
+        btnSubmit = findViewById(R.id.btnSimpan);
+
+        if (aksi.equals("Update")){
+            btnSubmit.setText("Update");
+            edtNo.setText(nomor);
+            edtNo.setFocusable(false);
+            edtName.setText(updated.getNama());
+            edtTanggal.setText(updated.getTanggal_lahir());
+            edtJenis.setText(updated.getJenis_kelamin());
+            edtAlamat.setText(updated.getAlamat());
+
+            Log.d("test", updated.getNama());
+        }
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatabaseHelper db = new DatabaseHelper(context);
+                Person person = new Person();
+                String label = btnSubmit.getText().toString();
+                if (label.equals("Simpan")){
+                    person.setNomor(Integer.parseInt(edtNo.getText().toString()));
+                    person.setNama(edtName.getText().toString());
+                    person.setTanggal_lahir(edtTanggal.getText().toString());
+                    person.setJenis_kelamin(edtJenis.getText().toString());
+                    person.setAlamat(edtAlamat.getText().toString());
+                    db.insert(person);
+                    Intent move = new Intent(context, MainActivity.class);
+                    context.startActivity(move);
+                }
+                if (label.equals("Update")){
+                    person.setNomor(Integer.parseInt(edtNo.getText().toString()));
+                    person.setNama(edtName.getText().toString());
+                    person.setTanggal_lahir(edtTanggal.getText().toString());
+                    person.setJenis_kelamin(edtJenis.getText().toString());
+                    person.setAlamat(edtAlamat.getText().toString());
+                    db.update(person);
+                    Intent move = new Intent(context, MainActivity.class);
+                    context.startActivity(move);
+                }
+            }
+        });
     }
 }
